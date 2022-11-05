631558e177cf1b2eab11bec55711ccaae78b192c
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2022-11-05 01:15:57 +0000

    update

--- main/w7-x_standard.py
+++ main/w7-x_standard.py
@@ -20,9 +20,9 @@ from simsopt.geo import CurveLength, CurveCurveDistance, \
 ##################################################################################################
 
 # Weight on the curve lengths in the objective function.
-LENGTH_WEIGHT = Weight(1e-6)
+LENGTH_WEIGHT = Weight(1e-8)
 # Threshold and weight for the coil-to-coil distance penalty in the objective function:
-CC_THRESHOLD = 0.5
+CC_THRESHOLD = 0.3
 CC_WEIGHT = 1000
 # Threshold and weight for the curvature penalty in the objective function:
 CURVATURE_THRESHOLD = 5.
@@ -39,8 +39,8 @@ filename = "/home/joaobiu/PIC/vmec_equilibria/W7-X/Standard/wout.nc"
 OUT_DIR = "./output/"
 os.makedirs(OUT_DIR, exist_ok=True)
 # Number of iterations to perform:
-ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
-MAXITER = 50 if ci else 400
+#ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
+MAXITER = 200
 
 ##################################################################################################
 ######################################## MAGNETIC SURFACE ########################################
@@ -48,20 +48,20 @@ MAXITER = 50 if ci else 400
 
 ntheta = 32
 nphi = 32
-s = SurfaceRZFourier.from_wout(filename, range="full torus", ntheta=ntheta, nphi=nphi)  #range = 'full torus', 'field period', 'half period'
+s = SurfaceRZFourier.from_wout(filename, range="half period", ntheta=ntheta, nphi=nphi)  #range = 'full torus', 'field period', 'half period'
 
 # Number of unique coil shapes:
-ncoils = 3
+ncoils = 4
 # Major radius for the initial circular coils:
 R0 = s.get_rc(0, 0)  #get_rc(0,0) dá nos o raio grande da superfície
 # Minor radius for the initial circular coils:
-R1 = 2.5
+R1 = 1.5
 
 # Number of Fourier modes describing each Cartesian component of each coil:
-order = 2
+order = 5
 
 # Criar curvas e correntes iniciais
-base_curves = create_equally_spaced_curves(ncoils, s.nfp,stellsym=True, R0=R0, R1=R1, order=order)
+base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=R0, R1=R1, order=order)
 base_currents = [Current(1.0) * 1e5 for i in range(ncoils)]
 # Since the target field is zero, one possible solution is just to set all
 # currents to 0. To avoid the minimizer finding that solution, we fix one
@@ -96,9 +96,9 @@ Jmscs = [MeanSquaredCurvature(c) for c in base_curves]
 JF = Jf \
     + LENGTH_WEIGHT * sum(Jls) \
     + CC_WEIGHT * Jccdist \
-    + CS_WEIGHT * Jcsdist \
     + CURVATURE_WEIGHT * sum(Jcs) \
     + MSC_WEIGHT * sum(QuadraticPenalty(J, MSC_THRESHOLD) for J in Jmscs)
+    # + CS_WEIGHT * Jcsdist 
 
 def fun(dofs):
     JF.x = dofs
@@ -111,7 +111,7 @@ def fun(dofs):
     kap_string = ", ".join(f"{np.max(c.kappa()):.1f}" for c in base_curves)
     msc_string = ", ".join(f"{J.J():.1f}" for J in Jmscs)
     outstr += f", Len=sum([{cl_string}])={sum(J.J() for J in Jls):.1f}, ϰ=[{kap_string}], ∫ϰ²/L=[{msc_string}]"
-    outstr += f", C-C-Sep={Jccdist.shortest_distance():.2f}, C-S-Sep={Jcsdist.shortest_distance():.2f}"
+    outstr += f", C-C-Sep={Jccdist.shortest_distance():.2f}"
     outstr += f", ║∇J║={np.linalg.norm(grad):.1e}"
     print(outstr)
     return J, grad
@@ -122,11 +122,11 @@ curves_to_vtk(curves, OUT_DIR + f"curves_opt_short")
 pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
 s.to_vtk(OUT_DIR + "surf_opt_short", extra_data=pointData)
 
-dofs = res.x
-LENGTH_WEIGHT *= 0.1
-res = minimize(fun, dofs, jac=True, method='L-BFGS-B', options={'maxiter': MAXITER, 'maxcor': 300}, tol=1e-15)
-curves_to_vtk(curves, OUT_DIR + f"curves_opt_long")
-pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
-s.to_vtk(OUT_DIR + "surf_opt_long", extra_data=pointData)
+#dofs = res.x
+#LENGTH_WEIGHT *= 0.1
+#res = minimize(fun, dofs, jac=True, method='L-BFGS-B', options={'maxiter': MAXITER, 'maxcor': 300}, tol=1e-15)
+#curves_to_vtk(curves, OUT_DIR + f"curves_opt_long")
+#pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
+#s.to_vtk(OUT_DIR + "surf_opt_long", extra_data=pointData)
 
 #plot(curves + [s], engine="mayavi")
