1947aac03670baea4e911994b3f5f89b44f61159
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-04-01 17:44:13 +0100

    test_curvecws_dofs concluded

--- tests/test_curvecws_dofs.py
+++ tests/test_curvecws_dofs.py
@@ -1,21 +1,49 @@
-from simsopt.geo import CurveCWSFourier, CurveRZFourier
-from simsopt.geo import SurfaceRZFourier
+import matplotlib.pyplot as plt
+import numpy as np
+from simsopt.geo import CurveCWSFourier, SurfaceRZFourier, CurveXYZFourier
 
-# CREATE CURVECWSFOURIER
-s = SurfaceRZFourier.from_nphi_ntheta(61, 62, "full torus", 1)
+###################################################
+###################################################
+##################CurveCWSFourier##################
+###################################################
+###################################################
+
+# CWS
+s = SurfaceRZFourier.from_nphi_ntheta(150, 150, "full torus", 1)
 R = s.get_rc(0, 0)
-s.set_dofs([R, 4, 4])
+s.set_dofs([R, 1, 1])
 
-c_cws = CurveCWSFourier(s.mpol, s.ntor, s.x, 150, 0, s.nfp, s.stellsym)
+# CWS CURVE
+c_cws = CurveCWSFourier(s.mpol, s.ntor, s.x, 15, 0, s.nfp, s.stellsym)
+c_cws.set_dofs([1, 0, 0, 0]) # [th_l, th_c0, phi_l, phi_c0]
 
-c_cws.set_dofs([1, 0, 0, 0])
+###################################################
+###################################################
+##################CurveXYZFourier##################
+###################################################
+###################################################
 
-# CREATE CURVERZFOURIER
+c_xyz = CurveXYZFourier(15, 10)
+c_xyz.set("xc(0)", R)
+c_xyz.set("xc(1)", 1)
+c_xyz.set("yc(0)", 0)
+c_xyz.set("yc(1)", 0)
+c_xyz.set("zs(1)", 1)
 
-c_rz = CurveRZFourier(150, 1, 1, 0)
-c_rz.set_dofs([0, 0, 1, 0, 0, 0])
-print(c_rz.gamma())
-c_rz.plot()
-c_cws.plot()
 
-print(c_rz.get_dofs())
+gamma_diff = c_xyz.gamma()-c_cws.gamma()
+gammadash_diff = c_xyz.gammadash()-c_cws.gammadash()
+gammadashdash_diff = c_xyz.gammadashdash()-c_cws.gammadashdash()
+#dgamma_by_dcoeff_diff = c_xyz.dgamma_by_dcoeff() - c_cws.dgamma_by_dcoeff()
+
+print(sum(abs(gamma_diff)))
+print(sum(abs(gammadash_diff)))
+print(sum(abs(gammadashdash_diff)))
+#print(sum(abs(dgamma_by_dcoeff_diff)))
+
+# PLOTS
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+c_cws.plot(ax=ax,show=False, plot_derivative=True)
+c_xyz.plot(ax=ax, show=True, plot_derivative=False)
