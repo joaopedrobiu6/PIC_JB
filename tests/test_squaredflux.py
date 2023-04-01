f45532e13179b1d07c2917f1e0b12e4c9425e67d
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-04-01 19:22:10 +0100

    Errors detected...everywhere

--- tests/test_squaredflux.py
+++ tests/test_squaredflux.py
@@ -1,26 +1,8 @@
-from simsopt.geo import CurveCWSFourier, SurfaceRZFourier, CurveLength
+from simsopt.geo import CurveCWSFourier, SurfaceRZFourier, CurveLength, CurveXYZFourier
 from simsopt.objectives import SquaredFlux
 from simsopt.field import BiotSavart
 from simsopt.field import Current, Coil
 import matplotlib.pyplot as plt
-
-
-#########################################################################################################
-#########################################################################################################
-################################################DEF######################################################
-#########################################################################################################
-#########################################################################################################
-def showplots(surf1, surf2, curve):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-
-    R = s.get_rc(0, 0)
-    surf1.set_dofs([R, 4, 4])
-
-    surf1.plot(ax = ax, show=False, alpha=0.2)
-    surf2.plot(ax=ax, show=False, alpha=1)
-    curve.plot(ax=ax)
-
 
 # SURFACE INPUT FILES FOR TESTING
 circular_tokamak = "/home/joaobiu/simsopt_curvecws/tests/test_files/wout_circular_tokamak_reference.nc"
@@ -38,12 +20,48 @@ surf.set_dofs([R, 4, 4])
 c = CurveCWSFourier(surf.mpol, surf.ntor, surf.x, 150, 1, surf.nfp, surf.stellsym)
 c.set_dofs([1, 0, 0, 0, 0, 0, 0, 0])
 
+c_xyz = CurveXYZFourier(150, 10)
+c_xyz.set("xc(0)", R)
+c_xyz.set("xc(1)", 4)
+c_xyz.set("yc(0)", 0)
+c_xyz.set("yc(1)", 0)
+c_xyz.set("zs(1)", 4)
+
+
 current = Current(1E5)
-coils =  [Coil(c, current)]
+coils_cws =  [Coil(c, current)]
+coils_xys =  [Coil(c_xyz, current)]
 
-bs = BiotSavart(coils)
-Jf = SquaredFlux(s, bs)
-print(Jf.J())
-c.plot()
+bs_cws = BiotSavart(coils_cws)
+Jf_cws = SquaredFlux(s, bs_cws)
+squaredflux_cws = Jf_cws.J()
 
-showplots(surf, s, c)
+bs_xyz = BiotSavart(coils_xys)
+Jf_xyz = SquaredFlux(s, bs_xyz)
+squaredflux_xyz = Jf_cws.J()
+
+Bcoil_cws = bs_cws.B()
+Bcoil_xyz = bs_xyz.B()
+
+print("Bcoil_cws: \n", Bcoil_cws)
+print("Bcoil_cws shape: ", Bcoil_cws.shape)
+print("####################################################################")
+print("Bcoil_xyz: \n", Bcoil_xyz)
+print("Bcoil_cws shape: ", Bcoil_cws.shape)
+print("####################################################################")
+print("Bcoil difference: \n", Bcoil_cws - Bcoil_xyz)
+
+
+
+#PLOT
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+surf.plot(ax = ax, show=False, alpha=0.2)
+s.plot(ax=ax, show=False, alpha=1)
+
+c.plot(ax=ax, show=False)
+c_xyz.plot(ax=ax)
+#coils_cws[0].plot(ax=ax)
+#coils_xyz[0].plot(ax=ax)
+
+plt.show()
