3a077da1ad94c97f7c2cd0a46d76903a85eded6e
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-04-03 22:05:36 +0100

    fixed the bug related to the gamma derivatives

--- tests/test_curvecws_dofs.py
+++ tests/test_curvecws_dofs.py
@@ -14,7 +14,7 @@ R = s.get_rc(0, 0)
 s.set_dofs([R, 1, 1])
 
 # CWS CURVE
-c_cws = CurveCWSFourier(s.mpol, s.ntor, s.x, 150, 0, s.nfp, s.stellsym)
+c_cws = CurveCWSFourier(s.mpol, s.ntor, s.x, 50, 0, s.nfp, s.stellsym)
 c_cws.set_dofs([1, 0, 0, 0]) # [th_l, th_c0, phi_l, phi_c0]
 
 ###################################################
@@ -23,7 +23,7 @@ c_cws.set_dofs([1, 0, 0, 0]) # [th_l, th_c0, phi_l, phi_c0]
 ###################################################
 ###################################################
 
-c_xyz = CurveXYZFourier(150, 10)
+c_xyz = CurveXYZFourier(50, 10)
 c_xyz.set("xc(0)", R)
 c_xyz.set("xc(1)", 1)
 c_xyz.set("yc(0)", 0)
@@ -31,19 +31,19 @@ c_xyz.set("yc(1)", 0)
 c_xyz.set("zs(1)", 1)
 
 
-gamma_diff = c_xyz.gamma()-c_cws.gamma()
-gammadash_diff = c_xyz.gammadash()-c_cws.gammadash()
-gammadashdash_diff = c_xyz.gammadashdash()-c_cws.gammadashdash()
+gamma_diff = c_xyz.gamma() - c_cws.gamma()
+gammadash_diff = c_xyz.gammadash() - c_cws.gammadash()
+gammadashdash_diff = c_xyz.gammadashdash() - c_cws.gammadashdash()
 #dgamma_by_dcoeff_diff = c_xyz.dgamma_by_dcoeff() - c_cws.dgamma_by_dcoeff()
 
-print(sum(abs(gamma_diff)))
-print(sum(abs(gammadash_diff)))
-print(sum(abs(gammadashdash_diff)))
+print(gamma_diff)
+print(gammadash_diff)
+print(gammadashdash_diff)
 #print(sum(abs(dgamma_by_dcoeff_diff)))
 
 # PLOTS
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-c_cws.plot(ax=ax,show=False, plot_derivative=True)
-c_xyz.plot(ax=ax, show=True, plot_derivative=False)
+c_cws.plot(ax=ax,show=True, plot_derivative=True)
+#c_xyz.plot(ax=ax, show=True, plot_derivative=True)
