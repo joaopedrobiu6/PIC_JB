eb8eb8dbc9f4b77a787d676e5b379288d8cbaadb
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-05-18 14:39:25 +0100

    commited some updates

--- code/stage_two_cws.py
+++ code/stage_two_cws.py
@@ -8,54 +8,76 @@ from simsopt.geo import curves_to_vtk
 from simsopt.field import BiotSavart, Current, coils_via_symmetries
 from simsopt.geo import (
     CurveLength, CurveCurveDistance,
-    MeanSquaredCurvature, LpCurveCurvature, CurveCWSFourier
+    MeanSquaredCurvature, LpCurveCurvature, CurveCWSFourier, ArclengthVariation
 )
-
+    
 OUT_DIR = "./output/"
 os.makedirs(OUT_DIR, exist_ok=True)
 
 # Threshold and weight for the maximum length of each individual coil:
-LENGTH_THRESHOLD = 6.5
+LENGTH_THRESHOLD = 20
 #LENGTH_WEIGHT = 0.1
-LENGTH_WEIGHT = 0.1
+LENGTH_WEIGHT = 1e-8
 
 # Threshold and weight for the coil-to-coil distance penalty in the objective function:
-CC_THRESHOLD = 0.1
+CC_THRESHOLD = 0.1 
 #CC_WEIGHT = 1000
-CC_WEIGHT = 100000
+CC_WEIGHT = 1000
 
 # Threshold and weight for the curvature penalty in the objective function:
-CURVATURE_THRESHOLD = 6.5
+CURVATURE_THRESHOLD = 50
 #CURVATURE_WEIGHT = 0.1
-CURVATURE_WEIGHT = 0.01
+CURVATURE_WEIGHT = 1e-10
 
 # Threshold and weight for the mean squared curvature penalty in the objective function:
-MSC_THRESHOLD = 6
+MSC_THRESHOLD = 10
 #MSC_WEIGHT = 0.01
+MSC_WEIGHT = 1e-10
+
+ARCLENGTH_WEIGHT = 3e-8
+LENGTH_CON_WEIGHT = 0.1
+
+"""
+J=4.747e-01, Jf=2.533e-01, ⟨B·n⟩=4.5e-02, Len=sum([4.5, 4.1, 3.7, 3.4])=15.7, ϰ=[9.0, 12.3, 6.5, 17.8], ∫ϰ²/L=[11.2, 9.0, 7.9, 9.3], C-C-Sep=0.20
+
+LENGTH_THRESHOLD = 6.5
+LENGTH_WEIGHT = 0.01
+
+CC_THRESHOLD = 0.15
+CC_WEIGHT = 100
+
+CURVATURE_THRESHOLD = 6.5
+CURVATURE_WEIGHT = 0.01
+
+MSC_THRESHOLD = 8
 MSC_WEIGHT = 0.01
+"""
+
 
 # SURFACE INPUT FILES FOR TESTING
-wout = '/home/joaobiu/simsopt_curvecws/examples/2_Intermediate/inputs/input.LandremanPaul2021_QH'
+wout = '/home/joaobiu/simsopt_curvecws/examples/3_Advanced/input.axiTorus_nfp3_QA_final'
 
 MAXITER = 1000 
-minor_radius_factor_cws = 1.9
-ncoils = 6
-order = 12 # order of dofs of cws curves
-quadpoints = 18 * order
-ntheta = 32
-nphi = 32
+ncoils = 4
+order = 10 # order of dofs of cws curves
+quadpoints = 200 #13 * order
+ntheta = 50
+nphi = 42
 
 # CREATE SURFACES
 s = SurfaceRZFourier.from_vmec_input(wout, range="half period", ntheta=ntheta, nphi=nphi)
 s_full = SurfaceRZFourier.from_vmec_input(wout, range="full torus", ntheta=ntheta, nphi=int(nphi*2*s.nfp))
+#cws = SurfaceRZFourier.from_nphi_ntheta(nphi, ntheta, "half period", s.nfp)
+#cws_full = SurfaceRZFourier.from_nphi_ntheta(int(nphi*2*s.nfp), ntheta, "full torus", s.nfp)
+
 cws = SurfaceRZFourier.from_vmec_input(wout, range="half period", ntheta=ntheta, nphi=nphi)
 cws_full = SurfaceRZFourier.from_vmec_input(wout, range="full torus", ntheta=ntheta, nphi=int(nphi*2*s.nfp))
 
 cws.extend_via_normal(0.2)
 cws_full.extend_via_normal(0.2)
 
-
-R = s.get_rc(0, 0)
+#R = s.get_rc(0, 0)
+#minor_radius_factor_cws = 1 + 0.2/s.get_zs(1, 0)
 #cws.set_dofs([R, s.get_zs(1, 0)*minor_radius_factor_cws, s.get_zs(1, 0)*minor_radius_factor_cws])
 #cws_full.set_dofs([R, s.get_zs(1, 0)*minor_radius_factor_cws, s.get_zs(1, 0)*minor_radius_factor_cws])
 
@@ -70,7 +92,7 @@ for i in range(ncoils):
         nfp=cws.nfp,
         stellsym=cws.stellsym,
     )
-    angle = (i+0.5)*(2*np.pi)/((2)*s.nfp*ncoils)
+    angle = (i + 0.5)*(2 * np.pi)/((2) * s.nfp * ncoils)
     curve_dofs = np.zeros(len(curve_cws.get_dofs()),)
     curve_dofs[0] = 1
     curve_dofs[2*order+2] = 0
@@ -80,7 +102,7 @@ for i in range(ncoils):
     curve_cws.fix(2*order+2)
     base_curves.append(curve_cws)
 base_currents = [Current(1)*1e5 for i in range(ncoils)]
-base_currents[0].fix_all()
+#base_currents[0].fix_all()
 
 coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
 
@@ -95,7 +117,7 @@ s_full.to_vtk(OUT_DIR + "surf_init", extra_data=pointData)
 cws_full.to_vtk(OUT_DIR + "cws_init")
 
 bs.set_points(s.gamma().reshape((-1, 3)))
-
+""" 
 Jf = SquaredFlux(s, bs)
 Jls = [CurveLength(c) for c in base_curves]
 Jccdist = CurveCurveDistance(curves, CC_THRESHOLD)
@@ -109,7 +131,20 @@ JF = (
     + CURVATURE_WEIGHT * sum(Jcs)
     + MSC_WEIGHT * sum(QuadraticPenalty(J, MSC_THRESHOLD) for J in Jmscs)
 )
-
+"""
+Jf = SquaredFlux(s, bs, local=True)
+Jls = [CurveLength(c) for c in base_curves]
+Jccdist = CurveCurveDistance(curves, CC_THRESHOLD, num_basecurves=len(curves))
+Jcs = [LpCurveCurvature(c, 2, CURVATURE_THRESHOLD) for i, c in enumerate(base_curves)]
+Jmscs = [MeanSquaredCurvature(c) for c in base_curves]
+Jals = [ArclengthVariation(c) for c in base_curves]
+J_LENGTH = LENGTH_WEIGHT * sum(Jls)
+J_CC = CC_WEIGHT * Jccdist
+J_CURVATURE = CURVATURE_WEIGHT * sum(Jcs)
+J_MSC = MSC_WEIGHT * sum(QuadraticPenalty(J, MSC_THRESHOLD, f="max") for i, J in enumerate(Jmscs))
+J_ALS = ARCLENGTH_WEIGHT * sum(Jals)
+J_LENGTH_PENALTY = LENGTH_CON_WEIGHT * sum([QuadraticPenalty(Jls[i], LENGTH_THRESHOLD, f="max") for i in range(len(base_curves))])
+JF = Jf + J_CC + J_LENGTH_PENALTY + J_CURVATURE + J_ALS + J_MSC  # + J_LENGTH
 
 def fun(dofs):
     JF.x = dofs
